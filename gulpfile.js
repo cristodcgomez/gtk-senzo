356c9fda8cc5514e68bb2a7bb081c3855a5cb672
cristomc <cristo.caamana@gosupplyservices.com>
2017-11-23 10:37:30 +0100

    initial files for GTK theme, based on Paper

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,15 @@ var gulp = require('gulp');
 var sass = sass = require('gulp-ruby-sass');
 
 // Compile Our Sass
-gulp.task('sass', function() {
+gulp.task('sass-gtk', function() {
+  return gulp.src('gtk-3.0/gtk.scss')
+    .pipe(sass({
+      style: 'expanded'
+    }))
+    .pipe(gulp.dest('gtk-3.0'))
+});
+
+gulp.task('sass-shell', function() {
   return gulp.src('sass/gnome-shell.scss')
     .pipe(sass({
       style: 'expanded'
@@ -13,5 +21,5 @@ gulp.task('sass', function() {
 });
 
 gulp.task('default', function () {
-  gulp.watch('sass/*.scss', ['sass']);
+  gulp.watch('*/**.scss', ['sass-shell', 'sass-gtk']);
 });
